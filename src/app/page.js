d077d4f2c41fe87c459c84ce15cb69452637ccaa
Tanusree-Das <tnsrdas@gmail.com>
2025-04-05 21:34:12 +0100

    Changes

--- src/app/page.js
+++ src/app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
         <div className="flex flex-col items-center max-w-4xl">
           <h2 className="text-6xl font-bold mb-4 text-white text-center">NeoRD.AI: End to End AI Solutions</h2>
           <h3 className="text-2xl font-regular mb-4 text-white text-center">Empowering Your Business With Every Kind Of AI solutions</h3>
-          <Link href="/book-demo" 
+          <Link href="/booking" 
           className="inline-block px-6 py-3 bg-[#1DA1F2] text-white rounded-lg hover:bg-[#1991db] transition-colors font-medium">
             Book Demo<span className="ml-2"> &gt; </span>
           </Link>
